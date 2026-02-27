baec04584c2cc27a1f1f2e600fcfe9224881dee1
mwangangibonifaceML <mwangangiboniface581@gmail.com>
2026-02-27 22:44:55 +0700

    made changes to the dataloader.py file refactoring how the TensoDataset class validates the tensors

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-#%% 
+
 import numpy as np
 import minitorch
 from minitorch.activations.activations import ReLU, Sigmoid, Tanh, Softmax
 from minitorch.nn.layers import Sequential, Linear, Dropout, Flatten
 from minitorch.tensor.tensor import Tensor
-from minitorch.dataloaders.dataloader import Dataset, TensorDataset
+from minitorch.dataloaders.dataloader import DataLoader, TensorDataset
 from minitorch.losses.losses import MSE, BinaryCrossEntropy, SoftMaxCrossEntropy
 from minitorch.optimizers.optim import SGD
 from typing import Tuple
@@ -12,31 +12,27 @@ from typing import Tuple
 print("Importing minitorch...")
 # print("minitorch version:", minitorch.__version__)
 print("Import successful!\n")
-#%%
 
+x = Tensor(np.array([[2.0, 3.0, 4.6,7.0],
+                    [4.0,5.0,8.0,10.0],
+                    [5.6,7.0, 11.1,1.0],
+                    [2.0, 3.0,0.0,-1.0],
+                    [4.0,5.0,-2.0, -10.0],
+                    [5.6,7.0, 11.9,12.0]]), requires_grad=True)
+y = Tensor(np.array([1.0, 2.0, 3.0, 3.0, 4.0,5.0]), requires_grad=True)
 
-#%%
+ds = TensorDataset([x,y])
+dataloader = DataLoader(ds, batch_size=2, shuffle=True)
+
 
 def main():
     print("\nHello from minitorch!\n")
-    x = Tensor(np.array([[1,2,-3,4,-5,7], 
-                    [-6,7,8,-9,10,20],
-                    [1,2,-3,4,-5,7],
-                    [1,2,-3,4,-5,7]], dtype=np.float32), requires_grad=True)
-
-    # batch_size, in_features, out_features = x.shape[0], x.shape[1], x.shape[1]
-    # l1 = Linear(in_features,        out_features,       bias=True)
-    # l2 = Linear(out_features,       out_features * 2,   bias=True)
-    # l3 = Linear(out_features * 2,   out_features,       bias=True)
-    # model = Sequential([l1,l2,l3])
-    # out = model(x)
     
-    # optimizer = SGD(model.parameters())
-    # optimizer.step()
     
+    for batch_index, (inputs, targets) in enumerate(dataloader):
+        print(f'Batch Number: {batch_index+1}, inputs: {inputs} and targets: {targets}')
     
 if __name__ == "__main__":
     main()
     
-# %%
 
